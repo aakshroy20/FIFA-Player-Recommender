b99bd632fc4e44863eaf2010f3cd0bfcf58f7814
aakshroy20 <61085333+aakshroy20@users.noreply.github.com>
2020-04-27 06:05:48 +0100

    Add files via upload

--- catalog/views.py
+++ catalog/views.py
@@ -21,8 +21,14 @@ def index(request):
             data_frame=get_data_frame(df,position)
             recommended_list=Recommendation_System(data_frame,player_id,5)
             context={}
-            context['supporting_text']="Players having similar ability to {} are: ".format(name)
-            context['list_of_names']=recommended_list
+            #context['supporting_text']="Players having similar ability to {} are: ".format(name)
+            #context['list_of_names']=recommended_list
+            context['searchedName']=name
+            context['recommendedNames1']=recommended_list[0]
+            context['recommendedNames2']=recommended_list[1]
+            context['recommendedNames3']=recommended_list[2]
+#           context['recommendedNames4']=recommended_list[3]
+            context['recommendedNames5']=recommended_list[3]
             return render(request,'catalog/index.html',context)
         else:
             context={}
